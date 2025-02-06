e956ca809cfe7c70327ec0a907a63cec42cb7d0d
VIVPM <vivpm99@gmail.com>
2025-02-06 22:12:46 +0530

    changes6

--- src/Admin.js
+++ src/Admin.js
@@ -1,17 +1,17 @@
-import { NavLink } from "react-router-dom";
-import './Admin.css'
-const Admin=()=>{
-    
-    return(
-        <div>
-            <div className="form">
-                <NavLink className="link" to='/Addplace'>Add a new place</NavLink>
-                <br/>
-                <br/>
-                <br/>
-                <NavLink className="link" to='/Deleteplace'>Delete a place</NavLink>
-            </div>
+import { Link } from "react-router-dom";
+import './Admin.css';
+
+const Admin = () => {
+    return (
+        <div className="navbar1">
+            <Link className="nav-bar-link naav1" to="/Addplace">
+                Add a new place
+            </Link>
+            <Link className="nav-bar-link naav1" to="/Deleteplace">
+                Delete a place
+            </Link>
         </div>
-    )
-}
+    );
+};
+
 export default Admin;
