b788efa015d868bfe97049085e09064af27bbfb3
VIVPM <vivpm99@gmail.com>
2025-02-07 17:06:51 +0530

    changes11

--- src/Card.js
+++ src/Card.js
@@ -11,19 +11,28 @@ function KitchenSinkExample(props) {
     localStorage.setItem("place", JSON.stringify(item));
   };
 
+  const calculateAverageRating = (ratings) => {
+    if (!ratings || ratings.length === 0) return "No ratings";
+    const total = ratings.reduce((acc, rating) => acc + rating, 0);
+    return (total / ratings.length).toFixed(1); // Rounded to 1 decimal place
+  };
+
   return (
     <div className="placelist">
       <br />
       {places.map((place) => (
         <div key={place._id || place.Name}>  {/* Use place._id if available, else fallback to place.Name */}
-          <Card style={{ width: "18rem", border: "black" }}>
+          <Card style={{ width: "18rem", border: "black"}}>
             <Card.Body>
-              <Card.Title>{place.Name}</Card.Title>
+              <Card.Title>Name: {place.Name}</Card.Title>
             </Card.Body>
             <ListGroup className="list-group-flush">
-              <ListGroup.Item>{place.Location}</ListGroup.Item>
+              <ListGroup.Item>Location: {place.Location}</ListGroup.Item>
+              <ListGroup.Item>
+                Average Rating: {calculateAverageRating(place.Rating)}
+              </ListGroup.Item>
             </ListGroup>
-
+            <br/>
             <Card.Body>
               <button onClick={() => setPlace(place)}>
                 <Link className="button-55" to="/det">Explore more</Link>
